9d457f8ac1e3df148cc114c120ef46b3b1215eb4
Brigham Andersen <brighamband@gmail.com>
2023-02-18 20:32:03 -0700

    Add port

--- server/server.js
+++ server/server.js
@@ -57,4 +57,7 @@ app.post('/process-payment', async (req, res) => {
   }
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running now on port " + port)
+})
